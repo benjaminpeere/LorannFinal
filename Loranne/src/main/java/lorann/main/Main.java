4408c1030844a0b73b4864d214f42e697615bbaf
benjaminpeere <benjamin.peere@vacesi.fr>
2016-06-21 19:55:39 +0200

    class sound

--- Loranne/src/main/java/lorann/main/Main.java
+++ Loranne/src/main/java/lorann/main/Main.java
@@ -1,28 +1,15 @@
 package lorann.main;
 
-import java.io.File;
-
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.Clip;
 
 public class Main {
-
+int i= 0;
 	public static void main (String[] args){
 		@SuppressWarnings("unused")
 		lorann.view.ViewFrame frm = new lorann.view.ViewFrame();
+		@SuppressWarnings("unused")
+		lorann.view.Sound sound = new lorann.view.Sound();
 		
-		File Clap = new File("Music/SonLorann.wav");
-		PlaySound(Clap);
 	}
 	
-	static void PlaySound(File Sound){
-		try{
-			Clip clip = AudioSystem.getClip();
-			clip.open(AudioSystem.getAudioInputStream(Sound));
-			clip.start();
-			//Thread.sleep(clip.getMicrosecondLength()/1000);
-		}catch(Exception e){
-			
-		}
-	}
+	
 }
--- Loranne/src/main/java/lorann/view/Sound.java
+++ Loranne/src/main/java/lorann/view/Sound.java
@@ -1,1 +1,24 @@
+package lorann.view;
 
+import java.io.File;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+
+public class Sound {
+	public Sound(){
+		File Clap = new File("Music/SonLorann.wav");
+		PlaySound(Clap);
+	}
+	static void PlaySound(File Sound){
+		
+		try{
+			Clip clip = AudioSystem.getClip();
+			clip.open(AudioSystem.getAudioInputStream(Sound));
+			clip.start();
+			Thread.sleep(clip.getMicrosecondLength());
+		}catch(Exception e){
+			
+		}
+	}
+}
